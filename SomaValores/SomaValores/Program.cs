94bea791a28d24b832a8f9398495232109f13c65
Luiz Gustavo Gomes <guga2605@outlook.com>
2024-05-20 14:52:30 -0300

    Exercicio 2

---
+++ SomaValores/SomaValores/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1, num2, soma;
+
+
+Console.WriteLine("Digite o o primeiro valor da soma");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor da soma");
+num2 = int.Parse(Console.ReadLine());
+
+
+soma = num1 + num2;
+
+Console.WriteLine($"O valor da soma total dos dois fatores informados é de: {soma}");
