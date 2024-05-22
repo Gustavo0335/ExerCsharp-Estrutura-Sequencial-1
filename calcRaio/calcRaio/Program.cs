df31c7462d99ae2034ed4bfd55bcaa1a6967c16f
Luiz Gustavo Gomes <guga2605@outlook.com>
2024-05-22 08:59:12 -0300

    Exercicio 3

---
+++ calcRaio/calcRaio/Program.cs
@@ -0,0 +1,13 @@
+﻿double Ent , valc, T = 3.14159, raio, resultadoArredondado;
+int casasDecimais = 4;
+
+Console.WriteLine("Insira o valor do circulo");
+Ent = double.Parse(Console.ReadLine());
+
+valc = Ent * Ent;
+
+raio = valc * T;
+
+resultadoArredondado = Math.Round(raio, casasDecimais);
+
+Console.WriteLine($"{resultadoArredondado}");
